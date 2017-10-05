9a2e7c087b94a3b36669e531b69eb976c1105347
Tiago Cucki <tcucki@gmail.com>
2017-10-05 20:02:36 +0200

    Fixing projects and pom files

---
+++ cognitive-wizard-parent/cognitive-wizard-poc/src/main/java/com/cognitive/newswizard/poc/CognitiveWizardPOCApplication.java
@@ -0,0 +1,12 @@
+package com.cognitive.newswizard.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CognitiveWizardPOCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CognitiveWizardPOCApplication.class, args);
+	}
+}
